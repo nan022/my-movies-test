a89b5e5685e3f741629e169ef174edef4e71a756
nan022 <ronanda20ti@mahasiswa.pcr.ac.id>
2023-04-10 22:06:16 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
-import requests
-from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://nande:nande@cluster0.8kafc33.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbmymovietest
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -12,44 +21,22 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
-@app.route("/movie", methods=["POST"])
-def movie_post():
+@app.route("/homework", methods=["POST"])
+def homework_post():
     # sample_receive = request.form['sample_give']
-    url_receive = request.form['url_give']
-    star_receive = request.form['star_give']
+    name_receive = request.form['name_give']
     comment_receive = request.form['comment_give']
-
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get(url_receive, headers=headers)
-
-    soup = BeautifulSoup(data.text, 'html.parser')
-
-    # From here on, we will write the code for extracting data from meta tags
-
-    og_image = soup.select_one('meta[property="og:image"]')
-    og_title = soup.select_one('meta[property="og:title"]')
-    og_description = soup.select_one('meta[property="og:description"]')
-
-    image = og_image['content']
-    title = og_title['content']
-    description = og_description['content']
-    
     doc = {
-        'image': image,
-        'title': title,
-        'description': description,
-        'star': star_receive,
+        'name': name_receive,
         'comment': comment_receive,
     }
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg':'Comment Posted!'})
 
-    db.movies.insert_one(doc)
-
-    return jsonify({'msg':'POST request!'})
-
-@app.route("/movie", methods=["GET"])
-def movie_get():
-    movie_list = list(db.movies.find({}, {'_id': False}))
-    return jsonify({'movies': movie_list})
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'messages': message_list})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
